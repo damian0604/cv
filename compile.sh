386a4016d8843437040a00130022972c3ed6dd2a
damian0604 <d.c.trilling@uva.nl>
2018-11-16 22:28:43 +0100

    fixed line breaks

--- compile.sh
+++ compile.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 rm *.bbl
 rm *.aux
-pdflatex -interaction=nonstopmode cv.tex
+#pdflatex -interaction=nonstopmode cv.tex
+pdflatex cv.tex
 for f in *.aux
 do
     bibtex $f
 done
-pdflatex -interaction=nonstopmode cv.tex
-
+#pdflatex -interaction=nonstopmode cv.tex
+pdflatex cv.tex
 
